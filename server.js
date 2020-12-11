ef73854e282fb9dcf8be5d8ddfd5eabeef62c0d1
Josh Padilla <jdp237@nau.edu>
2020-12-11 11:23:21 -0700

    updated mongoose connection

--- server.js
+++ server.js
@@ -11,16 +11,25 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("./public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 //HTML Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 app.get("/exercise", (req, res) => {
@@ -34,44 +43,53 @@ app.get("/stats", (req, res) => {
 //API Routes
 app.get("/api/workouts", (req, res) => {
     Workout.find({})
-    .then(dbUser => {
-        res.json(dbUser);
-    })
-    .catch(err => {
-        res.json(err)
-    });
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err)
+        });
 });
 
 app.get("/api/workouts/range", (req, res) => {
     Workout.find({})
-    .then(dbUser => {
-        res.json(dbUser);
-    })
-    .catch(err => {
-        res.json(err)
-    });
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err)
+        });
 });
 
-app.post("/api/workouts", ({ body }, res) => {
+app.post("/api/workouts", ({
+    body
+}, res) => {
     Workout.create(body)
-    .then(dbUser => {
-        res.json(dbUser)
-    })
-    .catch(err => {
-        res.json(err)
-    });
+        .then(dbUser => {
+            res.json(dbUser)
+        })
+        .catch(err => {
+            res.json(err)
+        });
 });
 
-app.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body }})
-    .then(dbUser => {
-        res.json(dbUser);
-    })
-    .catch(err => {
-        res.json(err)
-    });
+app.put("/api/workouts/:id", ({
+    body,
+    params
+}, res) => {
+    Workout.findByIdAndUpdate(params.id, {
+            $push: {
+                exercises: body
+            }
+        })
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+        .catch(err => {
+            res.json(err)
+        });
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
+    console.log(`App running on http://localhost:${PORT}`);
 });
